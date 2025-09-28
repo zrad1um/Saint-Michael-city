483365fe2bf19b1831e3f4ec56a69e63f777cc35
Ruslan "zradium" Zhmyshenko <snusoedgaara@gmail.com>
2025-09-28 20:18:26 +0500

    Add files via upload

---
+++ build/server/chunks/ssr/src_app_ca777385._.js
@@ -0,0 +1,3 @@
+module.exports=[68758,a=>{a.v("/_next/static/media/favicon.9a427d2f.ico")},19362,a=>{"use strict";a.s(["default",()=>b]);let b={src:a.i(68758).default,width:256,height:255}}];
+
+//# sourceMappingURL=src_app_ca777385._.js.map
