79ccf9e8b6317f6ad9d4b55b1673ee5bed8bece3
Ruslan "zradium" Zhmyshenko <snusoedgaara@gmail.com>
2025-09-28 19:55:39 +0500

    Add files via upload

---
+++ build/server/app/favicon.ico/route.js
@@ -0,0 +1,8 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/[root-of-the-server]__a69459cc._.js")
+R.c("server/chunks/node_modules_next_dist_77fd8386._.js")
+R.c("server/chunks/[root-of-the-server]__b218749b._.js")
+R.c("server/chunks/node_modules_next_dist_03a3c8f9._.js")
+R.m(15934)
+R.m(73932)
+module.exports=R.m(73932).exports
