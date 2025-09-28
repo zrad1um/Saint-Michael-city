1dd4a6caa2f69b9a160145b04087271b5e566f14
Ruslan "zradium" Zhmyshenko <snusoedgaara@gmail.com>
2025-09-28 19:50:45 +0500

    Add files via upload

---
+++ build/server/middleware-build-manifest.js
@@ -0,0 +1,33 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/e60ef129113f6e24.js",
+      "static/chunks/69a52dc464b81630.js",
+      "static/chunks/turbopack-9189f62d2f891f8e.js"
+    ],
+    "/_error": [
+      "static/chunks/17722e3ac4e00587.js",
+      "static/chunks/69a52dc464b81630.js",
+      "static/chunks/turbopack-815ee97e2af62b17.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/c62b4372c26b460f.js",
+    "static/chunks/5564f493499345f1.js",
+    "static/chunks/7880f8283a6f3daf.js",
+    "static/chunks/8082ab48faca5ea1.js",
+    "static/chunks/turbopack-b6a23b3e2832b47b.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Kt09mZXDt14MBECMEIdxZAtQrJqvhLxmm98o4wDKwBE=\"\n}"
